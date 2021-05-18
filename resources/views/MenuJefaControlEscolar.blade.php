c199a55956abd4908ea4101a149039e720d10baa
raccpunk <66329111+raccpunk@users.noreply.github.com>
2021-05-18 12:06:01 -0500

    Add design

--- resources/views/MenuJefaControlEscolar.blade.php
+++ resources/views/MenuJefaControlEscolar.blade.php
@@ -1,27 +1,83 @@
 <link rel="dns-prefetch" href="//fonts.gstatic.com">
-<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.1/css/bootstrap.min.css"  rel="stylesheet">
-<div class="card-header">Menú de Documentos de la Jefa de Control Escolar</div>
+<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.1/css/bootstrap.min.css"    rel="stylesheet">
+<div class="card-header " >INICIO</div>
 <ul class="navbar-nav ml-auto">
-<div class="card-body">
 
-</div>
+
+<!DOCTYPE html>
+
+<html>
+<head>
+
+<style>
+body {
+    margin: 0;
+}
+
+ul {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    width: 19%;
+    background-color: #056897;
+    position: fixed;
+    height: 100%;
+    overflow: auto;
+}
+
+li a {
+    display: block;
+    color: #ffffff;
+    padding: 8px 0 8px 16px;
+    text-decoration: none;
+}
+
+li a.active {
+    background-color: #144e6a;
+    color: white;
+}
+
+li a:hover:not(.active) {
+    background-color: #f7d109;
+    color: rgb(0, 0, 0);
+}
+
+</style>
+</head>
+<body>
+
+<ul>
+    <!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<title>Bootstrap 4 Buttons with Icons</title>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+<!-- Including Font Awesome CSS from CDN to show icons -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+
+  <li><a href="{{url('/MenuJefaControlEscolar')}}"type="button" class="Active  "><i class="fa fa-file-word-o"></i>  Documentos de control escolar</button>
+  <li><a href="{{url('/Calificaciones')}}">Kardex</a></li>
+  <li><a href="{{url('/Docentes')}}">Reporte de plantilla docente</a></li>
+
+</ul>
+
+
+</body>
+</html>
+
+</body>
+</html>
 <div class="container">
     <div class="row">
       <div class="col text-center">
-<a href="{{url('/Calificaciones')}}" class="btn btn-success ">Kardex</a>
 
 
-                <a href="{{url('/AprovechamientoEsc')}}" class="btn btn-success ">Estadísticas de aprovechamiento escolar</a>
-
-
-                                      
-                                        <a href="{{url('/Docentes')}}" class="btn btn-success ">Reporte de plantilla docente.
-                                        </a>
-                                              </div>
-                                            </div>
-                                        </div>
-                                      </div>
-                                    </div>
                                 </div>
                               </div>
                             </div>
@@ -29,6 +85,7 @@
                       </div>
                     </div>
                 </div>
+
               </div>
             </div>
         </div>
--- resources/views/PlantillaDocente.blade.php
+++ resources/views/PlantillaDocente.blade.php
@@ -8,13 +8,153 @@
 <div class="container">
     <div class="row">
       <div class="col text-center">
-<a href="{{url('/docentes')}}" class="btn btn-success ">Descargar Reporte de plantilla docente.</a>
 
+    <!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<title>Bootstrap 4 Buttons with Icons</title>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+<!-- Including Font Awesome CSS from CDN to show icons -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 
+<select name="select">
+	<option selected="yes">Seleccionar Documento</option>
+    <option href="{{url('/personal')}}" >Reporte de plantilla docente</option>
+	<option href="{{url('/docentes')}}">Reporte de plantilla de personal. </option>
 
-<a href="{{url('/personal')}}" class="btn btn-success ">Descargar Reporte de plantilla de personal.</a>
-<input type="button" class= "btn btn-primary"onclick="history.back()" name="volver atrás" value="volver atrás">
-
+if
+</select>
+<hr>
+</div>
+</div> </div>
+</div>
     </div>
   </div>
 </div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<title>Bootstrap Simple Success Confirmation Popup</title>
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto|Varela+Round">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+<style>
+body {
+	font-family: 'Varela Round', sans-serif;
+}
+.modal-confirm {
+	color: #636363;
+	width: 325px;
+	font-size: 14px;
+}
+.modal-confirm .modal-content {
+	padding: 20px;
+	border-radius: 5px;
+	border: none;
+}
+.modal-confirm .modal-header {
+	border-bottom: none;
+	position: relative;
+}
+.modal-confirm h4 {
+	text-align: center;
+	font-size: 26px;
+	margin: 30px 0 -15px;
+}
+.modal-confirm .form-control, .modal-confirm .btn {
+	min-height: 40px;
+	border-radius: 3px;
+}
+.modal-confirm .close {
+	position: absolute;
+	top: -5px;
+	right: -5px;
+}
+.modal-confirm .modal-footer {
+	border: none;
+	text-align: center;
+	border-radius: 5px;
+	font-size: 13px;
+}
+.modal-confirm .icon-box {
+	color: #fff;
+	position: absolute;
+	margin: 0 auto;
+	left: 0;
+	right: 0;
+	top: -70px;
+	width: 95px;
+	height: 95px;
+	border-radius: 50%;
+	z-index: 9;
+	background: #82ce34;
+	padding: 15px;
+	text-align: center;
+	box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.1);
+}
+.modal-confirm .icon-box i {
+	font-size: 58px;
+	position: relative;
+	top: 3px;
+}
+.modal-confirm.modal-dialog {
+	margin-top: 80px;
+}
+.modal-confirm .btn {
+	color: #fff;
+	border-radius: 4px;
+	background: #82ce34;
+	text-decoration: none;
+	transition: all 0.4s;
+	line-height: normal;
+	border: none;
+}
+.modal-confirm .btn:hover, .modal-confirm .btn:focus {
+	background: #6fb32b;
+	outline: none;
+}
+.trigger-btn {
+	display: inline-block;
+	margin: 100px auto;
+}
+</style>
+</head>
+<body>
+<div class="text-center">
+	<!-- Button HTML (to Trigger Modal) -->
+    
+	<button href="#myModal" data-toggle="modal" type="button" class="btn btn-outline-success"><i class="fa fa-floppy-o"></i> Descargar</button>
+</div>
+
+<!-- Modal HTML -->
+<div id="myModal" class="modal fade">
+	<div class="modal-dialog modal-confirm">
+		<div class="modal-content">
+			<div class="modal-header">
+				<div class="icon-box">
+					<i class="material-icons">&#xE876;</i>
+				</div>
+				<h4 class="modal-title w-100">Exito al descargar documento !</h4>
+			</div>
+			<div class="modal-body">
+				<p class="text-center">Tu documento fue descargado de manera correcta </p>
+			</div>
+			<div class="modal-footer">
+				<button class="btn btn-success btn-block" data-dismiss="modal">OK</button>
+			</div>
+		</div>
+	</div>
+</div>
+</body>
+</html>
